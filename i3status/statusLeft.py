4b77f3236676083e05fdec3173adaaab00e144fa
Adam Perdeusz <a.gogler@gmail.com>
2017-04-17 22:12:17 +0200

    Base16 colors.

--- i3status/statusLeft.py
+++ i3status/statusLeft.py
@@ -1,14 +1,20 @@
 #!/bin/python3
 import alsaaudio
 from i3pystatus import Status
+from i3pystatus.weather import weathercom
 
 status = Status(standalone=True)
 
 status.register("clock", format="%a %-d %b %X WW%V",)
 
 status.register("weather",
-        location_code="PLXX0006",
-        format="Gdynia: {current_temp}, {current_wind}, H:{humidity}%"
+        format="Gdynia: {current_temp}{temp_unit}, {wind_direction}-{wind_speed}{wind_unit} H:{humidity}%",
+        colorize=True,
+        backend=weathercom.Weathercom(
+            location_code="PLXX0006",
+            units='metric',
+            ),
+
         )
 
 status.register("alsa",
